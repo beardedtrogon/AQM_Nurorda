a865323dfe0a1e4202800fbc2e898eefcaf406c2
Tengiz <tuatkan@gmail.com>
2019-08-22 14:24:18 +0600

    RTC and battery LED combined with CO2

--- smoke_sensor/smoke_sensor.ino
+++ smoke_sensor/smoke_sensor.ino
@@ -2,12 +2,25 @@
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+#define NUM_SAMPLES 10
+
 ThreeWire myWire(4,5,2);
 RtcDS1302<ThreeWire> Rtc(myWire);
+
+int led = 13;
+int sum = 0; //sum of samples taken
 int smokeA0 = A5;
 int sensorThres = 400;
- 
+
+unsigned char sample_count = 0; // current sample number
+float voltage = 0.0; // calculated voltage
+
 void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
   //to setup CO2 checker
   pinMode(smokeA0, INPUT);
   Serial.begin(9600);
@@ -20,6 +33,21 @@ void setup() {
 }
  
 void loop() {
+  while(sample_count < NUM_SAMPLES){
+    sum += analogRead(A2);
+    sample_count++;
+  }
+
+  voltage = ((float)sum / (float)NUM_SAMPLES * 5.015) / 1024.0;
+
+  if(voltage * 2 < 6.0){
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+  Serial.print(voltage * 2);
+  Serial.println(" V");
+  
   RtcDateTime now = Rtc.GetDateTime();
 
   printDateTime(now);
@@ -35,10 +63,10 @@ void loop() {
   if(analogSensor > 350){
     Serial.println("Dangerous level");
   }
+  sample_count = 0;
+  sum = 0;
   delay(1000);
 }
-
-#define countof(a) (sizeof(a) / sizeof(a[0]))
 
 void printDateTime(const RtcDateTime& dt){
     char datestring[20];
